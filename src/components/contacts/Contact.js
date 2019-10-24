de7bded74db1260ebd6cde4f20a3423febec94c7
jfuller957 <jfuller957@gmail.com>
2019-10-24 14:52:42 -0700

    edit contact component built, pulls contact info based on contact id

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Consumer } from '../../context';
-import { faCaretDown, faTimes } from '@fortawesome/free-solid-svg-icons';
+import {
+  faCaretDown,
+  faTimes,
+  faPencilAlt
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import axios from 'axios';
 
@@ -10,10 +15,15 @@ class Contact extends Component {
     showContactInfo: false
   };
 
-  onDeleteClick = (id, dispatch) => {
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => dispatch({ type: 'DELETE_CONTACT', payload: id }));
+  onDeleteClick = async (id, dispatch) => {
+    try {
+      await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    } catch (ex) {
+      dispatch({ type: 'DELETE_CONTACT', payload: id });
+    }
+
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
   };
 
   render() {
@@ -42,6 +52,17 @@ class Contact extends Component {
                   icon={faTimes}
                   style={{ cursor: 'pointer', float: 'right', color: 'red' }}
                 />
+                <Link
+                  to={`contact/edit/${id}`}
+                  style={{
+                    cursor: 'pointer',
+                    float: 'right',
+                    color: 'black',
+                    marginRight: '1rem'
+                  }}
+                >
+                  <FontAwesomeIcon icon={faPencilAlt} />
+                </Link>
               </h4>
               {showContactInfo ? (
                 <ul className="list-group">
